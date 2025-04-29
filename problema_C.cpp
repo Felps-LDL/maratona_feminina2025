dcf8d78a88f0a4fb98cebbb5f6a137888c6d64d9
felipe.lages.lima@gmail.com <felipe.lages.lima@gmail.com>
2025-04-29 17:06:41 -0300

    Problema C

---
+++ problema_C.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define optimize ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define endl '\n'
+#define f first
+#define s second
+#define dbg(x) cout << "x = " << x << endl;
+#define ll long long
+#define pii pair<int, int> 
+#define piii pair<pair<int,int>, int>
+#define ALL(x) x.begin(), x.end()
+#define INF 0x3f3f3f3f
+#define LINF 0x3f3f3f3f3f3f3f3fll
+ 
+using namespace std;
+
+int N, A, B, C;
+
+int main()
+{
+    optimize;
+
+    cin >> N;
+
+    while(N--)
+    {
+        cin >> A >> B >> C;
+
+        if(A != B && B == C) cout << A << endl;
+        else if(A == B && B != C) cout << C << endl;
+        else cout << B << endl;
+    }
+    
+    return 0;
+}
