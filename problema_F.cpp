dc69f95a0a007649eac06304486a3089de677d68
felipe.lages.lima@gmail.com <felipe.lages.lima@gmail.com>
2025-05-01 02:45:33 -0300

    Problema F

---
+++ problema_F.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define optimize ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define endl '\n'
+#define f first
+#define s second
+#define dbg(x) cout << "x = " << x << endl;
+#define ll long long
+#define pii pair<int, int> 
+#define piii pair<pair<int,int>, int>
+#define ALL(x) x.begin(), x.end()
+#define INF 0x3f3f3f3f
+#define LINF 0x3f3f3f3f3f3f3f3fll
+ 
+using namespace std;
+
+int N, K, P;
+
+int main()
+{
+    optimize;
+
+    cin >> N >> P >> K;
+
+    N--;
+
+    if(P + K % N > N) cout << (P + K % N) - N << endl;
+    else cout << P + K % N + 1 << endl;
+
+    return 0;
+}
